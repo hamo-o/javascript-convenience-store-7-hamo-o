27f4a09bdcbf517cc8526ffa39fe865c17bbb9c4
hamo-o <yheel369@gmail.com>
2024-12-23 03:52:33 +0900

    feat(Promotion): 기간 및 개수에 따른 프로모션 할인 적용 가능 개수 구하기

--- src/models/Promotion.js
+++ src/models/Promotion.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Promotion {
   #name;
 
@@ -19,6 +21,35 @@ class Promotion {
 
   getName() {
     return this.#name;
+  }
+
+  #isInPromotionDate() {
+    const today = MissionUtils.DateTimes.now();
+    return today >= this.#startDate && today <= this.#endDate;
+  }
+
+  #canGetPromotion(quantity) {
+    return quantity < this.#buy;
+  }
+
+  #getExtraFreeQuantity(lastCount) {
+    if (lastCount === this.#buy) return this.#get;
+    return 0;
+  }
+
+  #getMaxFreeQuantity(costCount, freeCount, extraCount) {
+    return { costCount, freeCount, extraCount };
+  }
+
+  calcMaxFreeQuantity(quantity) {
+    if (!this.#isInPromotionDate() || !this.#canGetPromotion(quantity)) {
+      return this.#getMaxFreeQuantity(quantity, 0, 0);
+    }
+    const set = this.#buy + this.#get;
+    const setCount = Math.floor(quantity / set);
+    const free = setCount * this.#get;
+    const last = quantity % set;
+    return this.#getMaxFreeQuantity(quantity - free, free, this.#getExtraFreeQuantity(last));
   }
 }
 
